deefbc0ac87c7b2abe5ab0e5e226764e2b641edb
Mateo <billordomateo02@gmail.com>
2022-07-03 21:18:32 -0300

    Comentarios en los metodos de appPaciente, mainPaciente, obraSocial y Paciente

--- TurnoFacil/src/Clases/Paciente.java
+++ TurnoFacil/src/Clases/Paciente.java
@@ -3,11 +3,14 @@ package Clases;
 import java.util.ArrayList;
 
 public class Paciente {
+
+	//ATRIBUTOS
 	private String nombre, apellido, direccion, telefono, email;
 	private int DNI, numAfiliado;
 	private ObraSocial obraSocial;
 	private ArrayList<Turno> turnos;
-	
+
+	//CONSTRUCTOR
 	public Paciente(String nombre, String apellido, String direccion, String telefono, String email, int DNI, int numAfiliado, ObraSocial obraSocial) {
 		this.nombre = nombre;
 		this.apellido = apellido;
@@ -19,7 +22,7 @@ public class Paciente {
 		this.obraSocial = obraSocial;
 		this.turnos = new ArrayList<Turno>();
 	}
-	
+	//GETTERS
 	public String getNombre() {
 		return nombre;
 	}
@@ -51,7 +54,7 @@ public class Paciente {
 	public ObraSocial getObraSocial() {
 		return obraSocial;
 	}
-	
+	//Con este metodo se agrega un turno a la lista de turnos del paciente
 	public void addTurno(Turno turno) {
 		turnos.add(turno);
 	}
